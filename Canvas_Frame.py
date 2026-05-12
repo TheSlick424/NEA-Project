3be862b342bb327d9fa5173954e066bf640f6c11
TheSlick424 <dslick424@gmail.com>
2026-05-12 19:51:23 +0100

    Added Random Node Generation
    I made it that the nodes are generated in random positions and no nodes overalp each other

--- Canvas_Frame.py
+++ Canvas_Frame.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from random import randint
+import math
 
 class CanvasFrame(tk.Frame):
     def __init__(self, parent):
@@ -9,4 +10,28 @@ class CanvasFrame(tk.Frame):
         self.canvas.pack(fill= "both", expand = True)
 
     def generate_random_graph(self, nodes):
-        self.canvas.create_oval(0, 0, 500, 500, fill="red")
+        self.canvas.delete("all")
+        cols = math.ceil(math.sqrt(nodes))
+        rows = math.ceil(nodes / cols)
+
+        cell_height = self.canvas.winfo_height() // rows
+        cell_width = self.canvas.winfo_width() // cols
+
+        node_count = 0
+
+        for row in range(rows):
+            for col in range(cols):
+                if node_count >= nodes:
+                    break
+
+                cell_x1 = col * cell_width
+                cell_y1 = row * cell_height
+                cell_x2 = cell_x1 + cell_width
+                cell_y2 = cell_y1 + cell_height
+
+                node_size = min(cell_height, cell_width) // 3
+
+                x = randint(cell_x1, cell_x2 - node_size)
+                y = randint(cell_y1, cell_y2 - node_size)
+                self.canvas.create_oval(x, y, x + node_size, y + node_size, fill = "red", outline= "red")
+                node_count += 1
